a6216d14baad800020a10254b15c591c3f748f70
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-23 12:05:34 +0800

    Update css variables

--- src/lib/Tab.svelte
+++ src/lib/Tab.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
-	export let active = false;
-
+	import { activeTab } from './TabStore';
+	export const tabName = '';
 	function onClick() {
-		active = true;
+		activeTab.set(tabName);
 	}
 </script>
 
-<button class="tab" on:click={onClick} class:active>
-	<slot />
+<button class="tab" on:click={onClick} class:active={$activeTab === tabName}>
+	{tabName}
 </button>
 
 <style>
 	.tab {
 		cursor: pointer;
-		border-right: 1px solid #ccc;
-		border-bottom: 1px solid #ccc;
+		border-right: 1px solid var(--color-inactive);
+		border-bottom: 1px solid var(--color-inactive);
 	}
 	.active {
 		cursor: default;
+		border-top: 1px solid var(--color-primary);
 		border-bottom: none;
+		color: var(--color-secondary);
 	}
 </style>
