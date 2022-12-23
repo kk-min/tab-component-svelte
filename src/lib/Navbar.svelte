65120322b98ab058d115b7961634b8dd24cacdee
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-23 11:50:09 +0800

    Add files

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+	export const tabs = [
+		{ name: 'Home', href: '/' },
+		{ name: 'Projects', href: '/projects' },
+		{ name: 'Contact', href: '/contact' }
+	];
+</script>
+
+<style>
+	.navbar {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+	}
+</style>
---
+++ src/lib/Tab.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let active = false;
+
+	function onClick() {
+		active = true;
+	}
+</script>
+
+<button class="tab" on:click={onClick} class:active>
+	<slot />
+</button>
+
+<style>
+	.tab {
+		cursor: pointer;
+		border-right: 1px solid #ccc;
+		border-bottom: 1px solid #ccc;
+	}
+	.active {
+		cursor: default;
+		border-bottom: none;
+	}
+</style>
