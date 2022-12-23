48a1e0ffbe5a7e72db224cc7a78de920d783bb93
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-23 12:36:26 +0800

    Add styling for TabContent.svelte

--- src/lib/TabContent.svelte
+++ src/lib/TabContent.svelte
@@ -2,6 +2,17 @@
 	import { activeTab } from './TabStore';
 </script>
 
-<h1>
-	{$activeTab === 'Home' ? 'Home' : $activeTab === 'Projects' ? 'Projects' : 'Contact'}
-</h1>
+<container>
+	<h1>
+		{$activeTab === 'Home' ? 'Home' : $activeTab === 'Projects' ? 'Projects' : 'Contact'}
+	</h1>
+</container>
+
+<style>
+	container {
+		margin-top: 3rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
