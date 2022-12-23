0a79173a6a80703b5b7f98ef7b368cddf23c2c05
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-23 12:34:26 +0800

    Add placeholder TabContent component

---
+++ src/lib/TabContent.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { activeTab } from './TabStore';
+</script>
+
+<h1>
+	{$activeTab === 'Home' ? 'Home' : $activeTab === 'Projects' ? 'Projects' : 'Contact'}
+</h1>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/Navbar.svelte';
+	import TabContent from '$lib/TabContent.svelte';
 </script>
 
 <Navbar />
+<TabContent />
